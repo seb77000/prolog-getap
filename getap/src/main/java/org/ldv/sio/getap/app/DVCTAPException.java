fb8843e5b4019b771c79fc4f7214e3a2bfbd87e8
sminot <sminot@btssio.vinci-melun.org>
2012-11-13 10:46:09 +0100

    Ajout de la class DVCTAPException

---
+++ getap/src/main/java/org/ldv/sio/getap/app/DVCTAPException.java
@@ -0,0 +1,11 @@
+package org.ldv.sio.getap.app;
+
+public class DVCTAPException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DVCTAPException(String message) {
+		super(message);
+	}
+
+}
